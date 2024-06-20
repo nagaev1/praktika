51cb04d08e40ff89df9391d8762e111e3e2e155e
nagaev1 <nagaevmix33@gmail.com>
2024-06-20 14:21:22 +0700

    some_changes

---
+++ resources/views/exercises/the_first_sign_of_equality_of_triangles.blade.php
@@ -0,0 +1,19 @@
+
+<div>
+    <h1 class="mb-4">1. Выбери соответствующие рисунку утверждение</h1>
+    <img src="{{url('images/paralel-line.png')}}" alt="img" class=" w-[30rem] max-w-full mx-auto">
+    <h2>Данные прямые</h2>
+    <input type="radio" name="q1" class="correct" id="q1-a1" required>
+    <label for="q1-a1">Пересекаются</label><br>
+    <input type="radio" name="q1" id="q1-a2">
+    <label for="q1-a2">Не пересекаются</label><br>
+</div>
+<div>
+    <h1 >2. Если две прямые на плоскости, то они пересекаются</h1>
+    <input type="radio" name="q2" class="correct" id="q2-a1" required>
+    <label for="q2-a1">Неверно</label><br>
+    <input type="radio" name="q2" id="q2-a2">
+    <label for="q2-a2">Верно</label><br>
+</div>
+<button class="text-3xl font-bold border-2 rounded border-th-green p-4" type="submit" >Проверить</button>
+<div class=" " id="result">Результат: </div>
